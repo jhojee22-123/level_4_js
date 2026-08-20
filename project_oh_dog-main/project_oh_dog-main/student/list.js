8a069fccc8adb7a7902c2c8a6788bde8fa9c21d7
jhojee22-123 <jhojee22@gmail.com>
2026-08-20 13:12:17 +0900

    제이에스

---
+++ project_oh_dog-main/project_oh_dog-main/student/list.js
@@ -0,0 +1,13 @@
+// 오늘의 집 클론코딩 프로젝트 - 쇼핑몰 상품 주문영역 + 상세페이지
+//------------------------------------------------------------
+//small1 == big1
+//small2 == big2
+//1.---------------------------변수
+const smallThum = document.querySelectorAll('.small_thum img');
+const bigThum = document.querySelectorAll('.big_thum img');
+console.log(smallThum, bigThum);
+
+
+
+//2.---------------------------함수
+//3.---------------------------이벤트
