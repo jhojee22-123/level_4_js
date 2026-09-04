60116ecb1b945511ffa714697b4fcc82c112e75b
jhojee22-123 <jhojee22@gmail.com>
2026-09-04 11:42:12 +0900

    슬라이드 학습

--- movie_db_project_ex-main/movie_db_project_ex-main/scripts/index.js
+++ movie_db_project_ex-main/movie_db_project_ex-main/scripts/index.js
@@ -24,4 +24,46 @@ for(let i=0; i<9; i++){
     movieWrap1.appendChild(movieLi);
 }
 
+//--------------------------------------------------26/9/4
+//무비차트 + swiper
+// swiper-slide 반복 생성해서 swiper-wrapper 붙여넣기
+// swiper 플러그인 함수 연결은 swiper 최종 연결
+const chart_swiper =document.querySelector('.chart_swiper');
 
+const chart_swiper_func = new Swiper(chart_swiper,{
+    slidesPerView:2,
+    spaceBetween:10,
+}); //플러그인 연결
+
+for(let i=0; i<5; i++){
+    const chart_slide = document.createElement('div'); //생성
+    chart_slide.classList.add('swiper-slide');//클래스연결
+
+    chart_slide.innerHTML = `<p class="num">${moviesDB[i].id}위</p>`;
+    chart_slide.innerHTML += `<h3>${moviesDB[i].title}</h3>`;
+    chart_slide.innerHTML += `<p class="rating">${moviesDB[i].rating}</p>`;
+    chart_slide.innerHTML += `<p class="story">${moviesDB[i].plot}</p>`;
+    chart_slide.style.backgroundImage = `url(${moviesDB[i].poster})`;
+
+    chart_swiper.children[0].appendChild(chart_slide); //붙여넣기
+}
+//애니 슬라이드 연습
+const chart_swiper2 =document.querySelector('.chart_swiper2');
+
+const chart_swiper_func2 = new Swiper(chart_swiper2,{
+    slidesPerView:2,
+    spaceBetween:10,
+});
+
+for(let i=0; i<5; i++){
+    const chart_slide = document.createElement('div');
+    chart_slide.classList.add('swiper-slide');
+
+    chart_slide.innerHTML = `<p class="num">${anisDB[i].id}위</p>`;
+    chart_slide.innerHTML += `<h3>${anisDB[i].title}</h3>`;
+    chart_slide.innerHTML += `<p class="rating">${anisDB[i].rating}</p>`;
+    chart_slide.innerHTML += `<p class="story">${anisDB[i].plot}</p>`;
+    chart_slide.style.backgroundImage = `url(${anisDB[i].poster})`;
+
+    chart_swiper2.children[0].appendChild(chart_slide); //붙여넣기
+}
